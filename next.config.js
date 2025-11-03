8ad61f9017dfa99cc5accbb40d4fe72c141139a9
whyvel12 <whyvel12@gmail.com>
2025-11-03 14:23:19 +0700

    testing

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  // COMMENT dulu basePath dan assetPrefix untuk development
-  // basePath: '/landing-page-nextjs',
-  // assetPrefix: '/landing-page-nextjs/',
+  // PENTING: Ganti 'landing-page-nextjs' dengan nama repository Anda!
+  basePath: isProd ? '/landing-page-nextjs' : '',
+  assetPrefix: isProd ? '/landing-page-nextjs/' : '',
 }
 
 module.exports = nextConfig
